82e530912b7615f466e13094a80205e9469d989a
Yoshter <yoshter18@gmail.com>
2025-02-13 13:44:38 -0500

    PauseMenu Added

---
+++ player_ui.gd
@@ -0,0 +1,20 @@
+extends Control
+@onready var pauseMenu: Control = $PauseMenu
+var isVisible : bool = false
+var delay : float
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	delay += delta
+	if Input.is_action_just_pressed("Pause") and !isVisible and delay > 0.1:
+		pauseMenu.set_visible(true)
+		isVisible = true
+		delay = 0.0
+	if Input.is_action_just_pressed("Pause") and isVisible and delay > 0.1:
+		pauseMenu.set_visible(false)
+		isVisible = false
+		delay = 0.0
