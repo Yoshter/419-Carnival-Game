5551a86ab1adad067b901a3e48ca1c74a0af9c4a
Yoshter <yoshter18@gmail.com>
2025-02-28 11:24:35 -0500

    Main Menu Started

---
+++ scenes/main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+@onready var confirmQuit: Control = $ConfirmQuit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quit_pressed() -> void:
+	confirmQuit.set_visible (true) # Replace with function body.
+	
+
+
+func _on_no_pressed() -> void:
+	confirmQuit.set_visible (false) # Replace with function body.
+	
+
+
+
+func _on_yes_pressed() -> void:
+	get_tree().quit() # Replace with function body.
