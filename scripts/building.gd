bc96d0975384a377eedb76b89128391ba50868b4
bmurphy026 <brenmurphy026@gmail.com>
2025-03-05 16:14:49 -0500

    Added minigame win sound + logic for it in UFO minigame

--- scripts/building.gd
+++ scripts/building.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var scoreSound: AudioStreamPlayer = $scoreSound
+@onready var ticketGet = $"../../Ticket get"
 
 func _on_deathbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ufoPlayer"):
@@ -11,3 +12,6 @@ func _on_score_area_body_entered(body: Node2D) -> void:
 		if !body.isDead:
 			scoreSound.play(0.0)
 			GamesGlobal.ufoScoreUp()
+		if !body.isDead && GamesGlobal.ufoScore == 10:
+			ticketGet.play()
+			
