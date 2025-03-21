9971c5a1914484fae56ecba680caf21945781767
gum64 <77714864+gum64@users.noreply.github.com>
2025-03-21 14:13:08 -0400

    UFO Game Bug Fixes

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,6 +1,7 @@
 extends Button
 
 @export var nextScene : String
+@export var isQuit : bool = false
 
 #Custom funcs
 func changeSceneTo(nextScene) -> void:
@@ -8,4 +9,8 @@ func changeSceneTo(nextScene) -> void:
 	get_tree().change_scene_to_file(nextScene)
 
 func _on_pressed() -> void:
+	GamesGlobal.ufoScoreReset()
+	if isQuit:
+		PlayerGlobal.needsTeleport = true
+		PlayerGlobal.newPosition = Vector3(-107.4,1.266,-152.4)
 	changeSceneTo(nextScene)
