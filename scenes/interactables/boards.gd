a0c116a627cc988f4409f78b2fc434e38c13f660
bmurphy026 <brenmurphy026@gmail.com>
2025-03-31 16:19:44 -0400

    Failed Implementation of a lot of things
    -"Boards.gd" logic for board sound is not working
    -"Vent Thud trigger.gd" could not figure out how to do this
    -"Vent Ambience" in vent_closet not playing
    -"Reverb Test" added in vent_closet, but without vent ambience working I have no way to test it

--- scenes/interactables/boards.gd
+++ scenes/interactables/boards.gd
@@ -1,10 +1,12 @@
 extends Node3D
+@onready var boardBreak = $BoardBreak
 
 var inArea : bool = false
 
 func _process(delta: float) -> void:
 	if inArea:
 		if ItemsGlobal.checkItem("saw") and Input.is_action_pressed("ui_accept"):
+			boardBreak.play() #Not working
 			queue_free()
 
 func _on_board_area_body_entered(body: Node3D) -> void:
---
+++ scenes/vent_thud_trigger.gd
@@ -0,0 +1,7 @@
+extends Area3D
+@onready var Vent_Thud_Trigger_Shape = $CollisionShape3D
+@onready var thud = $Thud
+
+#I don't like GD script cole please do this for me thanks
+#I want this thud sound to play when the player enters the collision shape
+#it CANT be that hard
