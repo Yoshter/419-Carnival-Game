bf0cc2ac65f4c8d55fbdcd845e8fc8f3632fdd57
gum64 <77714864+gum64@users.noreply.github.com>
2025-04-15 19:52:01 -0400

    skeeble logic

--- scenes/minigameScenes/skeeble2/skeeble_ui.gd
+++ scenes/minigameScenes/skeeble2/skeeble_ui.gd
@@ -3,24 +3,45 @@ extends Control
 var throwPower : float = 0.0
 @onready var throwPowerBar: ProgressBar = $throwPowerBar
 var isAdding : bool = true
+var isThrowing : bool = false
+var roundNum : int = 1
+@onready var nextRoundButton: Button = $nextRoundButton
 
 func _process(delta: float) -> void:
-	if isAdding:
-		throwPower += delta * 30
-	if Input.is_action_just_pressed("ui_accept"):
-		if throwPower >= 5 and throwPower <= 20:
-			print("GOOMAH")
-		elif throwPower >= 30 and throwPower <= 45:
-			print("TANKO")
-		elif throwPower > 45 and throwPower < 55:
-			print("SHLUCK")
-		elif throwPower >= 55 and throwPower <= 70:
-			print("BUPKIS")
-		elif throwPower >= 80 and throwPower <= 95:
-			print("HEYOO")
-		else:
-			pass
-	if throwPower == 100:
-		#missanim plays
-		pass
-	throwPowerBar.set_value(throwPower)
+	if !isThrowing:
+		nextRoundButton.set_visible(true)
+		
+	if isThrowing:
+		nextRoundButton.set_visible(false)
+		if isAdding:
+			throwPower += delta * 30
+		if Input.is_action_just_pressed("ui_accept"):
+			if throwPower >= 5 and throwPower <= 20:
+				print("GOOMAH")
+				endRound()
+			elif throwPower >= 30 and throwPower <= 45:
+				print("TANKO")
+				endRound()
+			elif throwPower > 45 and throwPower < 55:
+				print("SHLUCK")
+				endRound()
+			elif throwPower >= 55 and throwPower <= 70:
+				print("BUPKIS")
+				endRound()
+			elif throwPower >= 80 and throwPower <= 95:
+				print("HEYOO")
+				endRound()
+			else:
+				pass
+		throwPowerBar.set_value(throwPower)
+		if throwPower >= 100:
+			#fail music playes
+			endRound()
+
+func endRound() -> void:
+	roundNum += 1
+	isThrowing = false
+	throwPower = 0.0
+
+func _on_next_round_button_pressed() -> void:
+	isThrowing = true
