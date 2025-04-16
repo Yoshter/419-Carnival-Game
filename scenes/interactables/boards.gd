414d6b6a4404b83100b5fcdc04aec70291e6d87f
gum64 <77714864+gum64@users.noreply.github.com>
2025-04-15 21:03:15 -0400

    magic

--- scenes/interactables/boards.gd
+++ scenes/interactables/boards.gd
@@ -1,5 +1,6 @@
 extends Node3D
 @onready var boardBreak: AudioStreamPlayer3D = $boardBreakSound
+@onready var boardBreakTimer: Timer = $boardBreakTimer
 
 var inArea : bool = false
 
@@ -8,7 +9,7 @@ func _process(delta: float) -> void:
 		if ItemsGlobal.checkItem("saw") and Input.is_action_pressed("ui_accept"):
 			boardBreak.play(0.0) #Not working
 			PlayerGlobal.setCanInteract(false)
-			queue_free()
+			boardBreakTimer.start()
 
 func _on_board_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
@@ -21,3 +22,6 @@ func _on_board_area_body_exited(body: Node3D) -> void:
 		if ItemsGlobal.checkItem("saw"):
 			PlayerGlobal.setCanInteract(false)
 		inArea = false
+
+func _on_board_break_timer_timeout() -> void:
+	queue_free()
