2decb0511f8448da45fa93ffcc9c187809f41b6e
gum64 <77714864+gum64@users.noreply.github.com>
2025-04-16 00:37:49 -0400

    skeeble

--- scenes/minigameScenes/skeeble2/skeeble_ui.gd
+++ scenes/minigameScenes/skeeble2/skeeble_ui.gd
@@ -6,6 +6,8 @@ var isAdding : bool = true
 var isThrowing : bool = false
 var roundNum : int = 1
 @onready var nextRoundButton: Button = $nextRoundButton
+@onready var winMenu: Control = $winMenu
+@onready var prizeButton: Button = $winMenu/prizeButton
 
 func _process(delta: float) -> void:
 	if !isThrowing:
@@ -17,19 +19,19 @@ func _process(delta: float) -> void:
 			throwPower += delta * 30
 		if Input.is_action_just_pressed("ui_accept"):
 			if throwPower >= 5 and throwPower <= 20:
-				print("GOOMAH")
+				GamesGlobal.skeebleScore += 10
 				endRound()
 			elif throwPower >= 30 and throwPower <= 45:
-				print("TANKO")
+				GamesGlobal.skeebleScore += 50
 				endRound()
 			elif throwPower > 45 and throwPower < 55:
-				print("SHLUCK")
+				GamesGlobal.skeebleScore += 100
 				endRound()
 			elif throwPower >= 55 and throwPower <= 70:
-				print("BUPKIS")
+				GamesGlobal.skeebleScore += 50
 				endRound()
 			elif throwPower >= 80 and throwPower <= 95:
-				print("HEYOO")
+				GamesGlobal.skeebleScore += 10
 				endRound()
 			else:
 				pass
@@ -37,6 +39,9 @@ func _process(delta: float) -> void:
 		if throwPower >= 100:
 			#fail music playes
 			endRound()
+	if roundNum == 4 and GamesGlobal.skeebleScore >= 175:
+		nextRoundButton.set_visible(false)
+		winMenu.set_visible(true)
 
 func endRound() -> void:
 	roundNum += 1
@@ -45,3 +50,10 @@ func endRound() -> void:
 
 func _on_next_round_button_pressed() -> void:
 	isThrowing = true
+
+func _on_prize_button_pressed() -> void:
+	PlayerGlobal.needsTeleport = true
+	PlayerGlobal.newPosition = Vector3(-26,1.6,8.1)
+	GamesGlobal.skeebleBeat = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().change_scene_to_file("res://scenes/gamestent.tscn")
--- scripts/globals/gamesGlobal.gd
+++ scripts/globals/gamesGlobal.gd
@@ -21,6 +21,7 @@ var towerHasJoystick : bool = false
 
 var dead : bool = false
 
+var skeebleScore : int = 0
 var skeebleBallNum : int = 1
 var skeebleChecked : bool = false
 var skeebleBeat : bool = false
