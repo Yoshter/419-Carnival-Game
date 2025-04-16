185de8caa851e2771f167984333685c2be1ab4f6
Yoshter <yoshter18@gmail.com>
2025-04-16 17:16:35 -0400

    target

--- scenes/minigameScenes/shootingRange/target.gd
+++ scenes/minigameScenes/shootingRange/target.gd
@@ -1,4 +1,7 @@
 extends Node3D
+@onready var red: Sprite3D = $"Target Sprites/Red"
+@onready var blue: Sprite3D = $"Target Sprites/Blue"
+@onready var purple: Sprite3D = $"Target Sprites/Purple"
 
 @export var targetLevel : int = 1
 var targetScore : int = 0
@@ -7,10 +10,13 @@ func _ready() -> void:
 	match targetLevel:
 		1:
 			targetScore = 50
+			red.set_visible(true)
 		2:
 			targetScore = 100
+			blue.set_visible(true)
 		3:
 			targetScore = 150
+			purple.set_visible(true)
 
 func score() -> void:
 	GamesGlobal.shootingRangeScore += targetScore
