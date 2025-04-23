02ce619f684d0e150b718684d152c5f2acc844e4
bmurphy026 <brenmurphy026@gmail.com>
2025-04-23 18:33:43 -0400

    Rain sound
    Rain for outside, Rain and thunder for power outage, quiet rain for inside

--- scripts/gamestent.gd
+++ scripts/gamestent.gd
@@ -5,10 +5,20 @@ var janitorDoorwayVisible : bool = false
 @onready var carnivalMusic: AudioStreamPlayer3D = $"audioSpeakers/Carnival music"
 var musicTime : float 
 @onready var mainLight: OmniLight3D = $Lights/OmniLight3D4
+@onready var rain_thunder: AudioStreamPlayer3D = $"audioSpeakers/Rain + Thunder"
+@onready var quiet_rain: AudioStreamPlayer3D = $QuietRain
+
 
 func _process(delta: float) -> void:
 	if PlayerGlobal.isBlackout:
 		carnivalMusic.stop()
+		quiet_rain.stop()
+		if !rain_thunder.playing:
+			rain_thunder.play(0.0)
+	if !PlayerGlobal.isBlackout:
+		rain_thunder.stop()
+		if !quiet_rain.playing:
+			quiet_rain.play(0.0)
 	if !janitorDoorwayVisible and GamesGlobal.checkBeatGame("ufo"):
 		janitorDoorwayBox.set_visible(true)
 		janitorDoorwayVisible = true
