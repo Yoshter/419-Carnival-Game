c6488c688bc02abfef3a6a43a493eab9370abf1c
gum64 <77714864+gum64@users.noreply.github.com>
2025-04-24 10:49:04 -0400

    bazinga

--- scenes/fungate.gd
+++ scenes/fungate.gd
@@ -1,12 +1,26 @@
 extends Node3D
 
 @export var itemToCheck : String 
+@onready var sprite1: AnimatedSprite3D = $sprite1
+@onready var sprite2: AnimatedSprite3D = $sprite2
+@onready var topSprite: Sprite3D = $topSprite
+@onready var gateBody: CollisionShape3D = $StaticBody3D/CollisionShape3D
+var isDown : bool = false
 
 func _on_pass_check_area_body_entered(body: Node3D) -> void:
 	print("in")
 	if body.is_in_group("player"):
 		print("player")
-		if ItemsGlobal.checkItem(itemToCheck):
-			queue_free()
+		if ItemsGlobal.checkItem(itemToCheck) and !isDown:
+			gateBody.call_deferred("set_disabled", true)
+			goDown()
+			
 		else:
 			print("failed")
+
+func goDown() -> void:
+	topSprite.set_visible(false)
+	sprite1.play("default")
+	sprite2.play("default")
+	#gateBody.disabled = true
+	isDown = true
--- scripts/globals/itemsGlobal.gd
+++ scripts/globals/itemsGlobal.gd
@@ -20,8 +20,8 @@ var towerTicketBool : bool = false
 var rangeTokenBool : bool = false
 var rangeTicketBool : bool = false
 
-var funPassLevel2 : bool = false
-var funPassLevel3 : bool = false
+var funPassLevel2 : bool = true
+var funPassLevel3 : bool = true
 var saw : bool = false
 var bbgun : bool = false
 var joystick : bool = false
