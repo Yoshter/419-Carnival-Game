fcc864cc9f8e25b6b50ffbf8aa5a34687c2dd005
gum64 <77714864+gum64@users.noreply.github.com>
2025-04-29 17:29:46 -0400

    replayable tower + quit button

--- scenes/minigameScenes/towerCrash/tower_main_menu.gd
+++ scenes/minigameScenes/towerCrash/tower_main_menu.gd
@@ -4,19 +4,31 @@ extends Control
 @onready var startButton: Button = $startButton
 @onready var winSound = $winSound
 var hasWon: bool = false
+@onready var quitButton: Button = $quitButton
 
 func _process(delta: float) -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-	if GamesGlobal.checkBeatGame("tower") and !hasWon:
+	
+	if GamesGlobal.checkBeatGame("tower") and !ItemsGlobal.checkItem("towerTicket"):
 		startButton.set_visible(false)
 		winSound.play(0.0)
-		hasWon = true
 		winMenu.set_visible(true)
-
+	else:
+		GamesGlobal.enemiesKilled = 0
+		startButton.set_visible(true)
+		quitButton.set_visible(true)
+		winMenu.set_visible(false)
+	print(hasWon)
+	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/minigameScenes/towerCrash/w1c1.tscn")
 
 func _on_prize_button_pressed() -> void:
+	PlayerGlobal.needsTeleport = true
+	PlayerGlobal.newPosition = Vector3(-26.36,1.266,-51.17)
+	get_tree().change_scene_to_file("res://scenes/gamestent.tscn")
+
+func _on_quit_b_utton_pressed() -> void:
 	PlayerGlobal.needsTeleport = true
 	PlayerGlobal.newPosition = Vector3(-26.36,1.266,-51.17)
 	get_tree().change_scene_to_file("res://scenes/gamestent.tscn")
--- scenes/minigameScenes/towerCrash/w_1c_1.gd
+++ scenes/minigameScenes/towerCrash/w_1c_1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _process(delta: float) -> void:
+	print(GamesGlobal.enemiesKilled)
 	if GamesGlobal.enemiesKilled >= 4:
 		GamesGlobal.towerBeat = true
 		get_tree().change_scene_to_file.call_deferred("res://scenes/minigameScenes/towerCrash/tower_main_menu.tscn")
